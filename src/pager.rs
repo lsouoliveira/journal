31f7c0b31275a856244e42cd89110f8b9ec606f1
Lucas Sousa <ls442336@gmail.com>
2025-03-16 15:40:15 -0300

    Remove colors when stdout is not terminal

--- src/pager.rs
+++ src/pager.rs
@@ -1,17 +1,33 @@
+use regex::Regex;
 use std::io::prelude::*;
+use std::io::IsTerminal;
 
 const PAGER_COMMAND: &str = "less";
 
 pub fn start_pager(output: &str) {
+    let sanitized_output = sanitize_output(output);
+
     let mut less_child = std::process::Command::new(PAGER_COMMAND)
         .stdin(std::process::Stdio::piped())
         .spawn()
         .unwrap();
 
     let mut stdin = less_child.stdin.take().unwrap();
-    let data = output.to_string();
 
-    std::thread::spawn(move || stdin.write_all(data.as_bytes()).unwrap());
+    std::thread::spawn(move || stdin.write_all(sanitized_output.as_bytes()).unwrap());
 
     less_child.wait().unwrap();
+}
+
+fn sanitize_output(output: &str) -> String {
+    if std::io::stdout().is_terminal() {
+        output.to_string()
+    } else {
+        remove_colors(output)
+    }
+}
+
+fn remove_colors(output: &str) -> String {
+    let ansi_escape_regex = Regex::new(r"\x1b\[[0-9;]*m").unwrap();
+    ansi_escape_regex.replace_all(output, "").to_string()
 }
